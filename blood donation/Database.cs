b8b15a36f90bc94ee8c322ea44cfcae17d02a3d6
Tonmoy chakrabarty joy <tonmoy.chakrabarty.joy01@gmail.com>
2026-06-09 11:04:40 +0600

    commit

--- blood donation/Database.cs
+++ blood donation/Database.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace blood_donation
 {
     internal class Database
     {
-        public static 
+        public static SqlConnection con = new SqlConnection(@"Data Source=TONMOY\SQLEXPRESS;Initial Catalog=TCJ_Academy;Integrated Security=True; TrustServerCertificate=True;");
+
     }
 }
